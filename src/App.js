161c056e714e13b9d2124f82f342beecda58da9c
chasin87 <yasinyuksek87@gmail.com>
2020-12-22 16:47:01 +0100

    added function to play and pause song

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ function App() {
   //State
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
