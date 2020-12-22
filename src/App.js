607aadec72f03dec84ccdadd795c1ae8a1c39a88
chasin87 <yasinyuksek87@gmail.com>
2020-12-22 16:24:50 +0100

    typo fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Player from "./components/Player";
 import Song from "./components/Song";
 
 //import Utility
-import data from ".util";
+import data from "./util";
 
 function App() {
   //State
