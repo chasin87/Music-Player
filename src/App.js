fb425f906466e4763f41fb931bc9016f711ada1a
chasin87 <yasinyuksek87@gmail.com>
2020-12-22 16:21:57 +0100

    created data music

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 //Import styles
 import "./styles/app.scss";
-
 //Adding Components
 import Player from "./components/Player";
 import Song from "./components/Song";
 
+//import Utility
+import data from ".util";
+
 function App() {
+  //State
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
       <Song />
